39067f073a955ec402246d41773ee7c1ef30245e
nagesh <bannikun@pcp182392pcs.timeinc.com>
2017-07-21 15:05:44 +0530

    loading image and css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
     var element = document.createElement('div');
     // Lodash, now imported by import 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    // Add the image to our existing div.
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
     return element;
 }
 document.body.appendChild(component());
