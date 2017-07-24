d8711e788f973cfd70cd96961e4a82c53b49f445
nagesh <bannikun@pcp182392pcs.timeinc.com>
2017-07-24 12:20:07 +0530

    output management added html-webpack-plugin clean-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-// import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+import _ from 'lodash';
+import printMe from './print.js';
 
-function component() {
+  function component() {
     var element = document.createElement('div');
-    // Lodash, now imported by import 
+    var btn = document.createElement('button');
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    // Add the image to our existing div.
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
-}
-document.body.appendChild(component());
+  }
+
+  document.body.appendChild(component());
